036dfc1b62e36523a3e56c7ef103829519580488
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2023-12-28 00:07:00 +0530

    backgrond update

--- src/components/backlgound/Background.tsx
+++ src/components/backlgound/Background.tsx
@@ -1,7 +1,7 @@
 
 const Background = (/*prop: any*/) => {
   return (
-    <div className="fixed top-0 left-0 w-screen h-screen bg-cover bg-center bg-no-repeat blur-3xl" style={{ backgroundImage: 'url("/darkbluebackgound.gif")' }} >
+    <div className="fixed top-0 left-0 w-screen h-screen bg-cover bg-center bg-no-repeat blur-3xl opacity-50" style={{ backgroundImage: 'url("/darkbluebackgound.gif")' }} >
       {/* {prop.children} */}
     </div>
   );
