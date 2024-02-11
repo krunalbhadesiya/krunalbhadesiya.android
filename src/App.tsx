cc657f6bcb82a54f072f1aaf09ca8af5e56eb77c
krunalbhadesiya <2101031000089@silveroakuni.ac.in>
2024-02-12 00:34:02 +0530

    Courser Effect

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,43 @@ import Home from "./pages/Home"
 import Projects from "./pages/Projects"
 import ContactUs from "./pages/ContactUs"
 import AboutUs from "./pages/AboutUs"
+import AnimatedCursor from 'react-animated-cursor';
 
 
 const App = () => {
   return (
     <Router>
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={10}
+        color='0, 68, 147'
+        outerAlpha={0.3}
+        innerScale={0.7}
+        outerScale={5}
+        outerStyle={{
+          borderColor: '#004493', // Example border color
+          borderWidth: '4px', // Example border width
+          borderStyle: 'double', // Example border style
+          borderRadius: '50%', // Example border radius
+          padding:"10px",
+        }}
+        clickables={[
+          'a',
+          'input[type="text"]',
+          'input[type="email"]',
+          'input[type="number"]',
+          'input[type="submit"]',
+          'input[type="image"]',
+          'label[for]',
+          'select',
+          'textarea',
+          'button',
+          '.link',
+          {
+            target: '.custom',
+          }
+        ]}
+      />
       <MyNavbar />
       <Background />
       <div className='relative z-10 min-h-screen'>
